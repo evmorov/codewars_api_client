57bdde856c01329005bcb28706550d4338b47241
Evgeny Morozov <evmorov@gmail.com>
2015-11-05 23:27:19 +0300

    Small refactor of require

--- lib/codewars_api.rb
+++ lib/codewars_api.rb
@@ -1,8 +1,22 @@
+begin
+  require 'pry'
+rescue LoadError
+  nil
+end
+
 module CodewarsApi
   BASE_URL = 'https://www.codewars.com'
   API_URL = '/api/v1'
 end
 
+class Hash
+  def delete!(key)
+    fetch(key)
+    delete(key)
+  end
+end
+
+require 'andand'
 require 'codewars_api/request_helper'
 require 'codewars_api/version'
 require 'codewars_api/client'
@@ -13,10 +27,3 @@ require 'codewars_api/train_specific_kata'
 require 'codewars_api/attempt_solution'
 require 'codewars_api/finalize_solution'
 require 'codewars_api/deferred_response'
-require 'andand'
-
-begin
-  require 'pry'
-rescue LoadError
-  nil
-end
