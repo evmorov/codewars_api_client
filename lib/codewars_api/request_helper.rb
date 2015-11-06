a80c9a9b57acf93932e92dbc7a643024bf13bfda
Evgeny Morozov <evmorov@gmail.com>
2015-11-06 22:24:18 +0300

    Remove unnecessary methods

--- lib/codewars_api/request_helper.rb
+++ lib/codewars_api/request_helper.rb
@@ -5,14 +5,6 @@ module CodewarsApi
     include HTTParty
     base_uri CodewarsApi::BASE_URL
 
-    def self.post_request(endpoint, request_options)
-      post(endpoint, request_options)
-    end
-
-    def self.get_request(endpoint, request_options = {})
-      get(endpoint, request_options)
-    end
-
     def self.add_api_key(request_options, api_key)
       request_options = request_options.dup
       request_options[:headers] = { 'Authorization' => api_key }
