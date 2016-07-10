3da48742ae28f38d3bcb2052507596b709b762be
Evgeny Morozov <evmorov@gmail.com>
2016-07-10 14:17:15 +0300

    Fix a blocker

--- lib/codewars_api/helpers/response_checker.rb
+++ lib/codewars_api/helpers/response_checker.rb
@@ -1,7 +1,9 @@
 module CodewarsApi
   class ResponseChecker
     def self.check_errors(response)
-      raise response.message unless response.code == 200
+      unless response.code.to_i == 200
+        raise "Response code is '#{response.code}'\n#{response.message}"
+      end
     end
   end
 end
