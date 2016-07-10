bba644ea1a38f9e715e19b4b2c006925ee7fc235
Evgeny Morozov <evmorov@gmail.com>
2016-07-10 13:59:42 +0300

    Raise if response not 200

--- lib/codewars_api/helpers/response_checker.rb
+++ lib/codewars_api/helpers/response_checker.rb
@@ -1,7 +1,7 @@
 module CodewarsApi
   class ResponseChecker
     def self.check_errors(response)
-      raise response.message if response.code == 500
+      raise response.message unless response.code == 200
     end
   end
 end
